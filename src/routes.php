520061d08ea65e75a903efdce1f1a53ecd791bea
Nicolás Silva <sovietfrance@gmail.com>
2017-01-05 20:10:43 -0300

    style(): Remove unused code

--- src/routes.php
+++ src/routes.php
@@ -1,20 +1,10 @@
 <?php
 // Routes
-
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
+$app->get('/', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
 $app->post('/login', function ($request, $response, $args) {
-    
-    //$loginParams = $request->getParsedBody();
-    // $username = $loginParams['username'];
-    // $password = $loginParams['password'];
-    //$response->getLogin()->params($loginParams['username'], $loginParams['password']);
     return $response;
-
 })->add($sendRequest);
